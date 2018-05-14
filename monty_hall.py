27880fc969df26da48e24c42353a233b524d376d
Santhosh <santhoshdasaraju@gmail.com>
2018-05-14 22:29:58 +0800

    comment section added
    Declared door variables

--- monty_hall.py
+++ monty_hall.py
@@ -6,7 +6,7 @@ Once you select a door, I will open one of the two you had not selected which do
 You will then have the opportunity to switch from the door you originally selected to an alternate door..
 """
 import random
-
+# Declaring doors for variables
 A = "A"
 B = "B"
 C = "C"
